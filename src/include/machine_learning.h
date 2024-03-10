46d86561899ad0b42b4dfc8f1f8f75013568e61a
Chen Liang <chen.liang.cn@outlook.com>
2024-03-10 20:39:40 +0000

    fixed header file

--- src/include/machine_learning.h
+++ src/include/machine_learning.h
@@ -20,8 +20,8 @@ typedef struct {
 
 data_t *read_from_data_file(char *file_name);
 
-double cost_function(double **X, double *y, double *theta, int num_train,
-		     int num_feat);
+double cost_function(double **X, double *y, double *theta, int num_feat,
+		     int num_train);
 
 double *gradient_descent(double **X, double *y, double *theta, float alpha,
 			 int num_feat, int num_train, int num_iters);
